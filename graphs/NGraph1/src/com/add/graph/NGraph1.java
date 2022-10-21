7b072acbca521aa03236be515184b34349befe79
danylobasarab <45040940+danylobasarab@users.noreply.github.com>
2022-10-21 23:54:25 +0300

    lab2

---
+++ graphs/NGraph1/src/com/add/graph/NGraph1.java
@@ -0,0 +1,14 @@
+
+package com.add.graph;
+
+
+public class NGraph1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Graph.getRoots();
+    }
+    
+}
